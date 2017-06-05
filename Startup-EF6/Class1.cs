d1b8be30b71f4cd9803fc36de65342d8883e7657
andrewhoi <andrew_hoi@yahoo.com>
2017-06-05 17:28:23 +0800

    Add xUnit

---
+++ Startup-EF6/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Startup_EF6.DbContext;
+using Xunit;
+
+namespace Startup_EF6
+{
+    public class Class1
+    {
+        [Fact]
+        public void OpenContext()
+        {
+            using (var dbContext = new NorthwindContext())
+            {
+
+            }
+        }
+    }
+}
